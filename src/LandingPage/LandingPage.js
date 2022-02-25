f6dad1c10a2d07f6581ae528eb55c24a844cdc32
Juergen Hoelsch <juergenhoelsch@gmail.com>
2022-02-25 17:10:35 +0100

    Format landing page

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Box from "@mui/material/Box";
+
 import LoginDialog from "./../register_login/LoginDialog";
 import RegisterDialog from "./../register_login/RegisterDialog";
 
