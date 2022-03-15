f68884778d3111474f5bc40b4bfa10055574bd8c
Juergen Hoelsch <juergenhoelsch@gmail.com>
2022-03-15 17:47:50 +0100

    Initial refactoring of CryptoEditDialog

--- src/CryptoEditDialog/CryptoEditDialog.js
+++ src/CryptoEditDialog/CryptoEditDialog.js
@@ -1,25 +1,24 @@
-import * as React from 'react';
+import * as React from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import FormControl from '@mui/material/FormControl';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import DeleteIcon from '@mui/icons-material/Delete';
-import IconButton from '@mui/material/IconButton';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
 
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
 
-import ProgressIcon from '../ProgressIcon/ProgressIcon';
-import SuccessDialog from '../SuccessDialog/SuccessDialog';
-import ErrorText from '../ErrorText/ErrorText';
+import ProgressIcon from "../ProgressIcon/ProgressIcon";
+import SuccessDialog from "../SuccessDialog/SuccessDialog";
+import ErrorText from "../ErrorText/ErrorText";
 
 export default function CryptoEditDialog(props) {
   const [assetsToUpdate, setAssetsToUpdate] = React.useState([]);
@@ -29,15 +28,15 @@ export default function CryptoEditDialog(props) {
   const [allAssetsDeleted, setAllAssetsDeleted] = React.useState(false);
 
   React.useEffect(() => {
-    setAssetsToUpdate(props.assets)
+    setAssetsToUpdate(props.assets);
   });
 
   const config = {
     headers: {
-       Authorization: "Bearer " + props.token,
-    }
+      Authorization: "Bearer " + props.token,
+    },
   };
-  
+
   const handleClose = () => {
     props.setOpenEditDialog(false);
     setIsSuccess(false);
@@ -47,14 +46,14 @@ export default function CryptoEditDialog(props) {
   };
 
   const handleAssetDelete = (cryptoName) => {
-    const newAssets = [...assetsToUpdate]
+    const newAssets = [...assetsToUpdate];
     for (let i = 0; i < newAssets.length; i++) {
       if (newAssets[i].CryptoName === cryptoName) {
         newAssets[i]["Deleted"] = true;
       }
     }
 
-    let numDeletedAssets = 0
+    let numDeletedAssets = 0;
     for (let i = 0; i < newAssets.length; i++) {
       if (newAssets[i]["Deleted"]) {
         numDeletedAssets++;
@@ -65,126 +64,152 @@ export default function CryptoEditDialog(props) {
       setAllAssetsDeleted(true);
     }
 
-    setAssetsToUpdate(newAssets)
+    setAssetsToUpdate(newAssets);
   };
 
   const handleAmountChange = (cryptoName, amount) => {
     if (amount <= 0) {
       amount = 0;
-      setError("Amount must be greater than zero")
+      setError("Amount must be greater than zero");
     } else {
-      setError()
+      setError();
     }
 
-    const newAssets = [...assetsToUpdate]
+    const newAssets = [...assetsToUpdate];
     for (let i = 0; i < newAssets.length; i++) {
       if (newAssets[i].CryptoName === cryptoName) {
-        newAssets[i].Amount = amount
-        newAssets[i]["Updated"] = true
-        break
+        newAssets[i].Amount = amount;
+        newAssets[i]["Updated"] = true;
+        break;
       }
     }
 
-    setAssetsToUpdate(newAssets)
+    setAssetsToUpdate(newAssets);
   };
 
   const handleUpdateAssets = () => {
     setIsLoading(true);
-    setError()
-    
-    const requests = []
-    
-    for (let i = 0; i < assetsToUpdate.length; i++) {
-      const cryptoName = assetsToUpdate[i]["CryptoName"]
-      
-      if ("Updated" in assetsToUpdate[i]) {
-        const amount = assetsToUpdate[i]["Amount"]
-        requests.push(axios.put('http://localhost:8080/assets/' + cryptoName, {amount: amount}, config))
-      } else if ("Deleted" in assetsToUpdate[i]) {
-        requests.push(axios.delete('http://localhost:8080/assets/' + cryptoName, config))
-      }
-    };
+    setError();
 
-    axios.all(requests)
+    const requests = [];
+
+    for (let i = 0; i < assetsToUpdate.length; i++) {
+      const cryptoName = assetsToUpdate[i]["CryptoName"];
+
+      if ("Updated" in assetsToUpdate[i]) {
+        const amount = assetsToUpdate[i]["Amount"];
+        requests.push(
+          axios.put(
+            "http://localhost:8080/assets/" + cryptoName,
+            { amount: amount },
+            config
+          )
+        );
+      } else if ("Deleted" in assetsToUpdate[i]) {
+        requests.push(
+          axios.delete("http://localhost:8080/assets/" + cryptoName, config)
+        );
+      }
+    }
+
+    axios
+      .all(requests)
       .then(() => {
         setIsLoading(false);
         setIsSuccess(true);
         props.fetchAssets();
       })
-      .catch(err => {
+      .catch((err) => {
         setIsLoading(false);
         setIsSuccess(false);
 
         if (err.response && err.response.data && err.response.data.error) {
-          setError(err.response.data.error)
+          setError(err.response.data.error);
         } else {
-          setError(err.message)
+          setError(err.message);
         }
       });
   };
 
+  if (isSuccess) {
+    return (
+      <SuccessDialog
+        message={"Successfully updated Assets"}
+        open={props.open}
+        handleClose={handleClose}
+      />
+    );
+  }
+
   return (
     <>
-      <Dialog open={props.open} onClose={handleClose} fullWidth={true} maxWidth={"xs"}>
-      {!isSuccess && <DialogTitle>Edit your Assets</DialogTitle>}
+      <Dialog
+        open={props.open}
+        onClose={handleClose}
+        fullWidth={true}
+        maxWidth={"xs"}
+      >
+        <DialogTitle>Edit your Assets</DialogTitle>
         <DialogContent>
-        { !isSuccess &&
           <List>
-            {
-              assetsToUpdate
-                .filter((a) => !("Deleted" in a))
-                .map((a) =>
-              <ListItem
-                secondaryAction={
-                  <IconButton onClick={() => handleAssetDelete(a.CryptoName)} edge="end" aria-label="delete">
-                    <DeleteIcon />
-                  </IconButton>
-                }
-              >
-                <Grid container spacing={2}>
-                  <Grid item xs={4}>
-                    <Paper elevation={0} sx={{mt: 4}}>{a.CryptoName}</Paper>
+            {assetsToUpdate
+              .filter((a) => !("Deleted" in a))
+              .map((a) => (
+                <ListItem
+                  secondaryAction={
+                    <IconButton
+                      onClick={() => handleAssetDelete(a.CryptoName)}
+                      edge="end"
+                      aria-label="delete"
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                      <Paper elevation={0} sx={{ mt: 4 }}>
+                        {a.CryptoName}
+                      </Paper>
+                    </Grid>
+                    <Grid item xs={8}>
+                      <TextField
+                        sx={{ width: 150 }}
+                        id="outlined-number"
+                        label="Amount"
+                        value={a.Amount}
+                        type="number"
+                        margin="normal"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        inputProps={{
+                          step: "0.1",
+                        }}
+                        onChange={(event) =>
+                          handleAmountChange(
+                            a.CryptoName,
+                            parseFloat(event.target.value)
+                          )
+                        }
+                      />
+                    </Grid>
                   </Grid>
-                  <Grid item xs={8}>
-                    <TextField
-                      sx={{width: 150}}
-                      id="outlined-number"
-                      label="Amount"
-                      value={a.Amount}
-                      type="number"
-                      margin="normal"
-                      InputLabelProps={{
-                        shrink: true,
-                      }}
-                      inputProps={{
-                        step: "0.1"
-                      }}
-                      onChange={(event) => handleAmountChange(a.CryptoName, parseFloat(event.target.value))}
-                    />
-                  </Grid>
-                </Grid>
-              </ListItem>
-              )
-            }
+                </ListItem>
+              ))}
           </List>
-          }
-          {allAssetsDeleted && !isSuccess && 
-            "All cryptos in your assets were marked for deletion. Click 'Apply' to confirm the deletion, or 'Cancel' for aborting it"
-          }
+          {allAssetsDeleted &&
+            "All cryptos in your assets were marked for deletion. Click 'Apply' to confirm the deletion, or 'Cancel' for aborting it"}
           {error && <ErrorText error={error} />}
           {isLoading && <ProgressIcon />}
-          {isSuccess && <SuccessDialog message={"Successfully updated Assets"} />}
         </DialogContent>
         <DialogActions>
-        {isSuccess && <Button onClick={handleClose}>Close</Button>}
-        {!isSuccess &&
           <>
             <Button onClick={handleClose}>Cancel</Button>
             <Button onClick={handleUpdateAssets}>Apply</Button>
           </>
-        }
         </DialogActions>
       </Dialog>
     </>
-  )
+  );
 }
