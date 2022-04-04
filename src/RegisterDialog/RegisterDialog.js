0159a3f48dd710f9d69927fc598783759c42f427
Juergen Hoelsch <juergenhoelsch@gmail.com>
2022-04-04 17:43:23 +0200

    Restructure RegisterDialog

--- src/RegisterDialog/RegisterDialog.js
+++ src/RegisterDialog/RegisterDialog.js
@@ -1,46 +1,46 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 
-import axios from 'axios';
+import axios from "axios";
 
-import validateMailAddress from './Mail';
-import ErrorText from '../ErrorText/ErrorText';
-import ProgressIcon from '../ProgressIcon/ProgressIcon';
-import SuccessDialog from '../SuccessDialog/SuccessDialog';
+import validateMailAddress from "./Mail";
+import ErrorText from "../ErrorText/ErrorText";
+import ProgressIcon from "../ProgressIcon/ProgressIcon";
+import SuccessDialog from "../SuccessDialog/SuccessDialog";
 
 function RegisterDialog() {
   const [open, setOpen] = React.useState(false);
   const [error, setError] = React.useState();
   const [isLoading, setIsLoading] = React.useState(false);
   const [isSuccess, setIsSuccess] = React.useState(false);
-  
+
   const [userName, setUserName] = React.useState("");
   const [userPassword, setUserPassword] = React.useState("");
   const [userMail, setUserMail] = React.useState("");
-  
+
   const [userNameError, setUserNameError] = React.useState("");
   const [mailError, setMailError] = React.useState("");
   const [passwordError, setPasswordError] = React.useState("");
-  
+
   const handleClickOpen = () => {
     setError();
-    setIsLoading(false)
-    setIsSuccess(false)
+    setIsLoading(false);
+    setIsSuccess(false);
     setOpen(true);
 
-    setUserName("")
-    setUserMail("")
-    setUserPassword("")
+    setUserName("");
+    setUserMail("");
+    setUserPassword("");
 
-    setUserNameError("")
-    setMailError("")
-    setPasswordError("")
+    setUserNameError("");
+    setMailError("");
+    setPasswordError("");
   };
 
   const handleClose = () => {
@@ -56,109 +56,114 @@ function RegisterDialog() {
       return;
     }
 
-    const user = {name: userName, mail: userMail, password: userPassword};
+    const user = { name: userName, mail: userMail, password: userPassword };
 
-    setIsLoading(true)
-    setError()
+    setIsLoading(true);
+    setError();
 
     // TODO: make URL configurable
-    axios.post('http://localhost:8080/users', user)
-        .then(() => {
-          setIsLoading(false)
-          setError(false);
-          setIsSuccess(true)
-        })
-        .catch(err => {
-          setIsLoading(false)
-          setIsSuccess(false)
+    axios
+      .post("http://localhost:8080/users", user)
+      .then(() => {
+        setIsLoading(false);
+        setError(false);
+        setIsSuccess(true);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setIsSuccess(false);
 
-          if (err.response && err.response.data && err.response.data.error) {
-            setError(err.response.data.error)
-          } else {
-            setError(err.message)
-          }
-        });
+        if (err.response && err.response.data && err.response.data.error) {
+          setError(err.response.data.error);
+        } else {
+          setError(err.message);
+        }
+      });
   };
+
+  if (isSuccess) {
+    return (
+      <SuccessDialog
+        message={"Successfully registered user"}
+        open={open}
+        handleClose={handleClose}
+      />
+    );
+  }
 
   return (
     <>
-      <Button sx={{mt:5, mr:5}} onClick={handleClickOpen}>Register</Button>
+      <Button sx={{ mt: 5, mr: 5 }} onClick={handleClickOpen}>
+        Register
+      </Button>
       <Dialog open={open} onClose={handleClose}>
-        {!isSuccess && <DialogTitle>Register</DialogTitle>}
+        <DialogTitle>Register</DialogTitle>
         <DialogContent>
-          { !isSuccess &&
-            <>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                label="User Name"
-                error={userNameError}
-                helperText={userNameError}
-                fullWidth
-                variant="standard"
-                onInput={e => {
-                  const providedUserName = e.target.value;
-                  if (providedUserName.length < 3) {
-                    setUserNameError("User name must have at least 3 characters")
-                  } else {
-                    setUserName(providedUserName)
-                    setUserNameError("")
-                  }
-                }}
-              />
-              <TextField
-                margin="dense"
-                id="email"
-                label="Email Address"
-                type="email"
-                error={mailError}
-                helperText={mailError}
-                fullWidth
-                variant="standard"
-                onInput={e => {
-                  const providedUserMail = e.target.value;
-                  if (!validateMailAddress(providedUserMail)) {
-                    setMailError("Invalid mail address provided")
-                  } else {
-                    setUserMail(providedUserMail);
-                    setMailError("")
-                  }
-                }}
-              />
-              <TextField
-                margin="dense"
-                id="password"
-                label="Password"
-                error={passwordError}
-                helperText={passwordError}
-                type="password"
-                fullWidth
-                variant="standard"
-                onInput={e => {
-                  const providedUserPassword = e.target.value;
-                  if (providedUserPassword.length < 8) {
-                    setPasswordError("Password must have at least 8 characters")
-                  } else {
-                    setUserPassword(providedUserPassword);
-                    setPasswordError("");
-                  }
-                }}
-              />
-            </>
-          }
+          <>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="User Name"
+              error={userNameError}
+              helperText={userNameError}
+              fullWidth
+              variant="standard"
+              onInput={(e) => {
+                const providedUserName = e.target.value;
+                if (providedUserName.length < 3) {
+                  setUserNameError("User name must have at least 3 characters");
+                } else {
+                  setUserName(providedUserName);
+                  setUserNameError("");
+                }
+              }}
+            />
+            <TextField
+              margin="dense"
+              id="email"
+              label="Email Address"
+              type="email"
+              error={mailError}
+              helperText={mailError}
+              fullWidth
+              variant="standard"
+              onInput={(e) => {
+                const providedUserMail = e.target.value;
+                if (!validateMailAddress(providedUserMail)) {
+                  setMailError("Invalid mail address provided");
+                } else {
+                  setUserMail(providedUserMail);
+                  setMailError("");
+                }
+              }}
+            />
+            <TextField
+              margin="dense"
+              id="password"
+              label="Password"
+              error={passwordError}
+              helperText={passwordError}
+              type="password"
+              fullWidth
+              variant="standard"
+              onInput={(e) => {
+                const providedUserPassword = e.target.value;
+                if (providedUserPassword.length < 8) {
+                  setPasswordError("Password must have at least 8 characters");
+                } else {
+                  setUserPassword(providedUserPassword);
+                  setPasswordError("");
+                }
+              }}
+            />
+          </>
           {error && <ErrorText error={error} />}
           {isLoading && <ProgressIcon />}
-          {isSuccess && <SuccessDialog message={"Successfully registered user"} />}
         </DialogContent>
         <DialogActions>
-        {isSuccess && <Button onClick={handleClose}>Close</Button>}
-        {!isSuccess &&
-          <>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={handleClickRegister}>Register</Button>
-          </>
-        }
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClickRegister}>Register</Button>
         </DialogActions>
       </Dialog>
     </>
